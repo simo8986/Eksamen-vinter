99d9c5c81463c8ff7c6f5df93109ab531ca164be
simo8986 <simo8986@student.eadania.dk>
2018-12-16 17:24:32 +0100

    Design Opgaver
    Færdige design opgaver

--- designopgaver.php
+++ designopgaver.php
@@ -11,12 +11,19 @@
         </div>
         <h1>Opgaver</h1>
         <h2>Illustrator</h2>
-            <p>Vi havde gennemgang på tavlen, hvor vi selv skulle følge med på vores pc. Der lavede vi følgende figurer. </p>
+            <p>Vi havde gennemgang på tavlen, hvor vi selv skulle følge med på vores pc. Der lavede vi disse illustrationer </p>
+        <a href="filer/design/illustratoropgaver.pdf" download>Download illustrator opgaver filen her</a>
         <h2>Photoshop</h2>
             <p>Vi skulle gentage hvad der blev lavet på tavlen, og her lavede vi følgende</p>
+            <a href="filer/design/photoshop.pdf" download>Download photoshop opgaver filen her</a>
+        <h2>Billede manipulation</h2>
+            <p>Vi fik en hel dag til at lave vores egen billedmanipulation. Den eneste grænse var at det skulle være noget med at komme igennem en ramme. Jeg lavede denne</p>
+            <img src="billeder/design/ramme.jpg" alt="Billedmanipulation">
         <h2>Farver og typografi</h2>
             <p>I forlængelse af oplægget omkring farver og typografi skulle vi lave vores eget farvehjul. Vi skulle også farve disse ’spøgelser’ med de farver vi troede passede til de forskellige humører spøgelserne havde fået. Til sidst skulle vi parre dem med en typografi der passede.</p>
+            <img src="billeder/design/spoelser.png" alt="farver og typografi eksempel">
         <h2>Gestaltlove kompendie</h2>
             <p>Efter et oplæg omkring gestaltlove skulle vi selv lave eksempler på dem, som til sidst kunne samles i et kompendie. Vi udviklede dette powerpoint og pdf dokument, med illustrationer lavet af gruppen.</p>
+            <a href="filer/design/Gestaltlove.pdf" download>Download gestaltlove kompendie filen her</a>
 	</body>
 </html>
